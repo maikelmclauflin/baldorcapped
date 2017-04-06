6b532afe66fbad5f4ae7cc994a52e4f874bc2746
mmclau14 <michael.j.mclaughlin20@gmail.com>
2017-04-05 22:57:25 -0500

    added default values for visualizations added stats readout

--- app/Stacks/singleton.jsx
+++ app/Stacks/singleton.jsx
@@ -20,6 +20,7 @@ class Stack extends React.Component {
         const offset = this.props.offset;
         const lastOnly = this.props.lastOnly;
         const firstOnly = this.props.firstOnly;
+        let winning = 0;
         data.forEach((first, i1, data) => {
             const root_first = root[i1];
             if (firstOnly && i1) {
@@ -29,25 +30,33 @@ class Stack extends React.Component {
                 const length = i2 + 1;
                 const root_last = root[i1 + 1 + i2];
                 const root_delta = root_last.y - root_first.y;
+                const gains = last.y - first.y - root_delta;
+                const positive = gains >= 0;
                 if (lastOnly && last !== list[list.length - 1]) {
                     return;
                 }
+                if (positive) {
+                    winning++;
+                }
                 blocks.push({
                     length: length,
-                    gains: last.y - first.y - root_delta,
+                    gains: gains,
+                    positive: positive,
                     offset: offset + i1
                 });
             });
         });
-        return (
+        return (<div>
+            <span>Capped Funds outperformed {winning} times out of {blocks.length} for a total win rate of {(winning / blocks.length) * 100}%</span>
             <div className="stack-container">
             {blocks.map((block, index) => {
                 return <Row
                     length={block.length}
                     offset={block.offset}
-                    positive={block.gains >= 0}
+                    positive={block.positive}
                     key={index} />
             })}
+            </div>
             </div>
         );
     }
